d01c6234d77665e09f467154d6fbfe100a1ecb3f
AbinayaS <abieditz005@gmail.com>
2024-04-21 16:52:28 +0530

    organised the home component

--- apppsg/src/App.js
+++ apppsg/src/App.js
@@ -1,22 +1,10 @@
+import React from 'react';
+import Home from './components/Home.jsx';
 import './App.css';
-import logo from './images/logo_psg4u.png';
 function App() {
   return (
     <div>
-      <nav>
-      <img src={logo} alt="PSG4U Logo" />
-        <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <div className = "home">
-        <h1>THE STUDENT'S SPACE</h1>
-        <i>Where all the needs are met</i>
-        <button>Login</button>
-      </div>
-    
+        <Home />
     </div>
   );
 }
---
+++ apppsg/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css';
+const Home = () => {
+  return (
+    <div>
+    <nav>
+    <img src="/images/logo_psg4u.png" alt="PSG4U Logo" />
+
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div className = "home">
+      <h1>THE STUDENT'S SPACE</h1>
+      <i>Where all the needs are met</i>
+      <button>Login</button>
+    </div>
+    </div>
+  )
+}
+
+export default Home
