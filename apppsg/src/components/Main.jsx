8329918c1f3630ee9aedde5ccb340e07077eedcf
bala227 <115527760+bala227@users.noreply.github.com>
2024-04-25 20:17:06 +0530

    myaccount

--- apppsg/src/components/Main.jsx
+++ apppsg/src/components/Main.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Main.css';
 import {Link} from "react-router-dom";
 import Footer from "./Footer";
 
 export const Main = () => {
+
+  const[show,setshow] = useState(false);
+  
+  const myacc =()=>{
+    if(show === true){}
+    else{   
+      setshow(true)
+      document.getElementById('sect').style.opacity = 0.3      
+    }
+  }
+  const close =()=>{
+    if(show === true){setshow(false)
+      document.getElementById('sect').style.opacity = 1  }
+    else{   
+          
+    }
+  }
+
   return (
-    <div className='main'>
+    <div className='main' id='main'>
       <nav className='navbar'>
       <img src="/images/logo_psg4u.png" alt="PSG4U Logo" className='homelogo'/>
         <ul>
           <img src="/images/homelogo.png" className='homeimg' alt='homeimg'/>
           <li>Home</li>
           <img src="/images/person.png" className='personimg' alt='personimg'/>
-          <li>My account</li>
+          <li onClick={myacc}>My account</li>
           <li className='logout'>Logout</li>
         </ul>
       </nav>
-      
+      <section id='sect'>
         <div className='studies'>
             <img src="/images/clip.png" className='clip' alt='clip'/>
             <div className='title'>Studies</div>
@@ -55,6 +73,22 @@ export const Main = () => {
             <p><Link to={'/afternoonmenu'} class="link">Afternoon menu</Link></p>    
         </div>
         <Footer />
+        </section>
+        
+        {
+          show?<div className='Myaccount'>
+          <img src="/images/close.png" className='closeimg' onClick={close} alt='closeimg'/>
+          <img src="/images/person.png" className='personaccount' alt='personimg'/>
+          <p className='myaccname'>Name</p>
+          <p className='myaccp'>Roll no : 22n2yy</p>
+          <p className='myaccp'>Email : 22n2yy@psgtech.ac.in</p>
+          <img src="/images/coin.png" className='coin' alt='coinimg'/>
+          <p className='points'>20 points</p>
+          <img src="/images/editicon.png" className='editicon' alt='editimg'/>
+          <p className='editdetails'>Edit_details</p>
+        </div>:null
+        }
+        
     </div>
   )
 }
